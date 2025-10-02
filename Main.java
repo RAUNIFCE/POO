e0a79bf0f93c2fa0dfa31b96e50c80d5b8b1a203
RUANIFCE <ruan.goncalves08@aluno.ifce.edu.br>
2025-10-02 13:51:52 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,56 +1,77 @@
 import java.util.ArrayList;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int[] idade= new int[5];//cria array de tamanho 5
-        String[] nomes ={"maria","jose","joao","pedro","miguel"};/* ou  String[] nomes = new String[4];*/
-        idade[0]=10;
-        idade[2]=25;
-        //System.out.println(idade[1]);
-        //System.out.println(nomes[4]);
-        /*for(String n:nomes){//percorrer lista de strings
-            System.out.println(n);
-        }*/
-        /*for (int i=0;i<5;i++){
-            System.out.println(idade[i]);
-        }*/
-        /*for(int n:idade){
-            System.out.println(n);
-        }*/
-        /*for(int i=0;i< idade.length;i++){//se nao souber o tamanho use .length
-            System.out.println(idade[i]);
-        }*/
-        ArrayList<String> frutas= new ArrayList<>();// a partir daqui nao acesso com frutas[i], mas com frutas.
-        frutas.add("Manga");
-        frutas.add("Goiaba");
-        frutas.add("Caju");
-
-        System.out.println(frutas.size());//retorna a quantidades de itens na lista de frutas
-        frutas.add(1,"Morango");
-        frutas.add(2,"Pêra");
-
-        for(String f:frutas){//percorrer lista de strings
-            System.out.println(f);
+        int temperaturas[]={22, 25, 19, 28, 26, 23, 30};
+        int soma=0;
+        for(int n:temperaturas){
+            soma=soma+n;
         }
-        frutas.set(0,"Melancia");//substitui a string naquela posição pela string passado no parametro
-        System.out.println("\nsubstituição\n");
-        for(String f:frutas){//percorrer lista de strings
-            System.out.println(f);
-        }
-        System.out.println("\n"+frutas.get(2)+"\n");//retorne o elemento na posição 2
-        //frutas.remove(0);//remove tal elemento da lista
-        //System.out.println(frutas.remove(0));/*mostra oq foi retirado*/
 
-        frutas.remove("Caju");
-        for(String f:frutas){//percorrer lista de strings
-            System.out.println(f);
+        int diamaiortemp=0;
+        for(int n:temperaturas){
+            if(n>=25){
+                diamaiortemp++;
+            }
         }
-        System.out.println(frutas.contains("Maracujá"));//ve se tem tal palavra na lista
-        System.out.println(frutas.contains("Goiaba"));//cuidado pq é key sensitive
-        frutas.clear();//apaga todos os elementos da lista
-        System.out.println(frutas.size());
-         System.out.println(frutas.indexOf("Goiaba"));//retorna indice de goiaba
+        System.out.println(soma+" é a soma das temperaturas  e "+diamaiortemp+" é o numero de dias com temperaturas maior de 24");
+        //Segunda questão
+        /* Confusões
+        ArrayList<String> nomesProdutos= new ArrayList<>();
+        nomesProdutos.add("Laptop");
+        nomesProdutos.add("Mouse");
+        nomesProdutos.add("Teclado");
+        nomesProdutos.add("Monitor");
+        System.out.println(nomesProdutos.get(0));//imprime primero produto da lista
+        nomesProdutos.set(3,"Fone de Ouvido");
+        */
+         String[] nomesProdutos={"Laptop", "Mouse", "Teclado", "Monitor"};
+        System.out.println(nomesProdutos[0]);
+        nomesProdutos[nomesProdutos.length-1]="Fone de Ouvido";
+        for(int i=0;i< nomesProdutos.length;i++){
+            System.out.println(nomesProdutos[i]);
+        }
+        //Terceira questao
+        System.out.println("\n");
+         ArrayList<String> minhaRotina= new ArrayList<>();
+         minhaRotina.add("Acordar");
+         minhaRotina.add("Tomar café");
+         minhaRotina.add("Estudar Java");
+         minhaRotina.add(1,"Fazer exercícios");
+          for(String r:minhaRotina){//percorrer lista de strings
+            System.out.println(r);
+        }
+        System.out.println("\n");
+        //Quarta questao
+        minhaRotina.remove(2);
+        minhaRotina.remove("Tomar café");
+        System.out.println(minhaRotina.size());
+        for(String r:minhaRotina){//percorrer lista de strings
+            System.out.println(r);
+        }
+        System.out.println("\n");
+        //Quinta questao
+        ArrayList<Integer> numerosSorteados= new ArrayList<>();
+        numerosSorteados.add(10);
+        numerosSorteados.add(14);
+        numerosSorteados.add(18);
+        numerosSorteados.add(20);
+        numerosSorteados.add(30);
+        numerosSorteados.add(40);
+        System.out.println(numerosSorteados.get(0));
+        System.out.println(numerosSorteados.indexOf(40));
+        System.out.println(numerosSorteados.get(numerosSorteados.size()-1));
+        System.out.println("\n");
+        //Sexta questao
+        ArrayList<Double> gastosMensais= new ArrayList<>();
+        gastosMensais.add(125.50);
+        gastosMensais.add(300.00);
+        gastosMensais.add(700.75);
+        gastosMensais.add(1000.10);
+        System.out.println(gastosMensais.contains(50));
+        System.out.println(gastosMensais.isEmpty());
+        gastosMensais.clear();
+        System.out.println(gastosMensais.isEmpty());
+        
     }
 }
