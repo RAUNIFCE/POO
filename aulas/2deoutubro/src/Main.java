35d491da79c0c151b6225a4e3d1da0692c4c9350
RUANIFCE <ruan.goncalves08@aluno.ifce.edu.br>
2025-10-02 19:02:38 -0300

    Add files via upload

---
+++ aulas/2deoutubro/src/Main.java
@@ -0,0 +1,23 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Carro c1= new Carro();//cria variavel tipo carro, o new é usado pra criar objetos
+        c1.cor="azul";
+        c1.ano=2025;
+        c1.quilometragem=5;
+        c1.chassi="AFGG678";
+        //c1.acelerar();
+        //c1.frear();
+        c1.exibirInfo();
+
+        Carro c2= new Carro();
+        c2.cor="cinza";
+        c2.ano=2021;
+        c2.quilometragem=150;
+        c2.chassi="ZSDFAFGGg";
+        c2.exibirInfo();
+
+
+    }
+}
