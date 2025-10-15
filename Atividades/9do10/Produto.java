5f23586f417c73e9796020ac72c4cb9d1443c3cc
RUANIFCE <ruan.goncalves08@aluno.ifce.edu.br>
2025-10-14 21:51:47 -0300

    Add files via upload

---
+++ Atividades/9do10/Produto.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+public class Produto{
+    String nome;
+    double valor;
+    int qtde;
+    public Produto(String n,double v,int q){
+        this.nome=n;
+        this.valor=v;
+        this.qtde=q;
+    }
+}
